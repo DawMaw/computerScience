94ed40b98bb01a979b3818e5e3c058ca6b07fa08
Jon Loe <jdl@example.com>
2022-06-28 08:37:31 +0630

    calculate the future value V3

--- chap02/futureValue.py
+++ chap02/futureValue.py
@@ -3,11 +3,13 @@
 # carried 10 years into the future
 
 print("This program calculates the future value of an investment")
-principal = eval(input("Enter the initial principal: "))
+
+payment = eval(input("Enter amount to invest each year: "))
 apr = eval(input("Enter the annual interest rate: "))
 years = eval(input("Enter the number of years: "))
 
+principal = 0.0
 for i in range(years):
-    principal = principal * (1 + apr)
+    principal = (principal + payment) * (1 + apr)
 
 print("The amount in", years, " years is:", principal)
